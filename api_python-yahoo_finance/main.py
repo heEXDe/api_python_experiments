fc67a01341bc894d91b314af7b091f24e10b7139
https://heEXDe@github.com <m.a.p.zaleski@protonmail.com>
2022-05-25 17:56:40 +0200

    small UI in main file

--- api_python-yahoo_finance/main.py
+++ api_python-yahoo_finance/main.py
@@ -1,13 +1,27 @@
 import requests
 import json
 
-def rest_fn():
-    key = input(f'\n Insert your Yahoo Finance API key: \n')
-    actio = input(f'\n Decide what you want to do: /n get - get the info \n')
-    if actio == 'get':
-        # api_url = "https://yfapi.net/v6/finance/quote/BTC-USD"
+def api_fn():
+    global key
+    if key == 0:
+        key = input(f'\n Insert your Yahoo Finance API key: \n')
+    actions = f'\n Choose an action: \n get - get the info \n v - veriosn of the program \n exit - exit the program'
+    print(actions)
+    inp = input()
+    if inp == 'get':
         response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"BTC-USD"})
-        return f'\n Yahoo Finance \n BTC-USD: \n STATUS CODE: \n {response.status_code} \n RESPONSE TEXT: \n {response.text} \n'
+        print(f'\n Yahoo Finance (BTC-USD) \n Status code: \n {response.status_code} \n Response JSON deserialized to what data type: \n {type(json.loads(response.text))} \n ')
+        # RESPONSE JSON: \n {response.json()} \n RESPONSE DESERIALIZED: \n {json.loads(response.text)}
+        api_fn()
+    elif inp == 'v':
+        print('1.0.0')
+        api_fn()
+    elif inp == 'exit':
+        print(f'Closing the program.')
+    else:
+        api_fn()
 
-print(rest_fn())
+
+key = 0
+api_fn()
 
