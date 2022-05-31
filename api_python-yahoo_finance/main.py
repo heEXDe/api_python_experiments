5a027961df992a5a7e1d204ff036639144391b8a
https://heEXDe@github.com <m.a.p.zaleski@protonmail.com>
2022-05-31 23:19:44 +0200

    main.py: separate function for processing response

--- api_python-yahoo_finance/main.py
+++ api_python-yahoo_finance/main.py
@@ -8,34 +8,42 @@ def api_fn():
         key = input(f'\n Insert your Yahoo Finance API key: \n')
     
     # choose an action
-    actions = f'\n Choose an action (no space at the end): \n get current price - get the info \n v - veriosn of the program \n exit - exit the program'
+    actions = f'\n Choose an action (no space at the end): \n gcp-btc - get current price of BTC/USD \n gcp-eth - get current price of ETH/USD \n v - current version of the program \n exit - exit the program'
     print(actions)    
     # btc_usd_dict = 0
     inp = input()
-    if inp == 'get current price':
+    if inp == 'gcp-btc':
         # Getting the response from API
-            response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"BTC-USD"})
-
-            # Get the current price
-            a =  list(json.loads(response.text).values())[0]
-            a = dict(a).values()
-            a = list(a)[0]
-            a = a[0]
-            a = a.get("regularMarketPrice")
-            # Printing the output
-            print(f'\n Yahoo Finance (BTC-USD) \n Current price: {a} [USD/BTC] \n')
-            
-            # Repeating the function
-            api_fn()    
+        response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"BTC-USD"})
+        # Printing the output
+        print(f'\n Yahoo Finance (BTC-USD) \n Current price: {proc_json(response, "regularMarketPrice")} [USD/BTC] \n')
+        # Repeating the function
+        api_fn()
+    elif inp == 'gcp-eth':
+        # Getting the response from API
+        response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"ETH-USD"})
+        # Printing the output
+        print(f'\n Yahoo Finance (BTC-USD) \n Current price: {proc_json(response, "regularMarketPrice")} [USD/ETH] \n')
+        # Repeating the function
+        api_fn()
     elif inp == 'v':
-            print('1.0.0')
-            api_fn()    
+        print('1.0.0')
+        api_fn()    
     elif inp == 'exit':
-            print(f'Closing the program.')    
+        print(f'Closing the program.')    
     else:
-            print(f'An error occurred. You probably entered the command incorrectly (space at the end ?).')
-            api_fn()
+        print(f'An error occurred. You probably entered the command incorrectly (space at the end ?).')
+        api_fn()
 
+
+def proc_json(in_json, goal):
+        # Get the current price
+        a =  list(json.loads(in_json.text).values())[0]
+        a = dict(a).values()
+        a = list(a)[0]
+        a = a[0]
+        a = a.get(goal)
+        return a
 
 key = 0
 api_fn()
