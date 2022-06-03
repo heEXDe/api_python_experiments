6a9fa23e8b8336d50870bfd6aefcb2a52bcee611
https://heEXDe@github.com <m.a.p.zaleski@protonmail.com>
2022-06-03 23:36:07 +0200

    changes in YahooFinance API main file and addinh a folder for IMDb API

--- api_python-yahoo_finance/main.py
+++ api_python-yahoo_finance/main.py
@@ -8,10 +8,10 @@ def api_fn():
         key = input(f'\n Insert your Yahoo Finance API key: \n')
     
     # choose an action
-    actions = f'\n Choose an action (no space at the end): \n btc-usd - get current info for BTC/USD pair \n eth-usd - get current info for ETH/USD pair \n v - current version of the program \n exit - exit the program'
+    actions = f'\n Choose an action (no space at the end): \n btc-usd - get current info for BTC/USD pair \n eth-usd - get current info for ETH/USD pair \n ixic-usd - get current info for IXIC/USD (NASDAQ) pair \n v - current version of the program \n exit - exit the program'
     print(actions)
     inp = input()
-    # if get current BTC price's been choosen
+    # if btc-usd's been choosen
     if inp == 'btc-usd':
         # Getting the response from API
         response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"BTC-USD"})
@@ -20,12 +20,20 @@ def api_fn():
         # print(f'\n SOMETHING EXTRA: \n {json.loads(response.text)} \n')
         # Repeating the function
         api_fn()
-    # if get current ETH price's been choosen
+    # if eth-usd's been choosen
     elif inp == 'eth-usd':
         # Getting the response from API
         response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"ETH-USD"})
         # Printing the output
         print(f'\n Yahoo Finance (ETH-USD) \n Current price: {proc_json(response, "regularMarketPrice")} [USD/ETH] \n Two hundred day average: {proc_json(response, "twoHundredDayAverage")} [USD/ETH]')
+        # Repeating the function
+        api_fn()
+    # if eth-usd's been choosen
+    elif inp == 'ixic-usd':
+        # Getting the response from API
+        response = requests.get("https://yfapi.net/v6/finance/quote", headers={'x-api-key': key}, params={"symbols":"^IXIC"})
+        # Printing the output
+        print(f'\n Yahoo Finance (^IXIC) \n Current price: {proc_json(response, "regularMarketPrice")} [USD/^IXIC] \n Two hundred day average: {proc_json(response, "twoHundredDayAverage")} [USD/^IXIC]')
         # Repeating the function
         api_fn()
     # if get version of the program's been choosen
